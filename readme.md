afadff626649eee500963cb567f5b2c58488a923
Jason Pan <jpan@box.com>
2020-05-17 22:15:42 -0400

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git Commands
+
+git init - create a new git repo
+git status - view the changes to your project code
+git add - add files to staging area
+git commit - Creates a new commit
+git log - view recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
 // entry -> output
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	},
+// Webpack Configuration Types -> Exporting a Function
+// Function gets called with arguments
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	const miniCssExtract = new MiniCssExtractPlugin({
+		filename: 'styles.css'
+	})
 
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}]
+		},
+		plugins: [
+			miniCssExtract
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		},
+	};
 };
